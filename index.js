c12539c6d62a848836faf18417d73cba5e61c3c0
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2025-01-05 18:29:21 +0100

    test

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-
-AFRAME.registerComponent('log', {
-    schema: {
-      message: {type: 'string'}
-    },
-  
-    init: function () {
-        console.log(this.data.message + " -> from " + this.el.localName);
-    },
-  
-    update: function () {
-      console.log("Updated -> from " + this.el.localName);
-    },
-  
-    remove: function () {
-      // Do something when the component or its entity is detached.
-    },
-  
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
-    }
+AFRAME.registerComponent('controller-event-handler', {
+  init: function () {
+    var el = this.el;
+    el.addEventListener('mouseenter', function () {
+      el.setAttribute('material', 'color', '#24CAFF');  
+    });
+    el.addEventListener('mouseleave', function () {
+      el.setAttribute('material', 'color', '#EF2D5E');  
+    });
+  }
 });
